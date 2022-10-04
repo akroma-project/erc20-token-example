7735bd2ea4b445e48989dc6a037123cb804147ce
Eric Polerecky <eric@polerecky.com>
2022-10-04 16:08:51 -0400

    chore: upgrade packages, add tests (#1)
    * chore: upgrade packages, add tests
    
    * fix: update ci/cd test paths

--- contracts/SampleToken.sol
+++ contracts/SampleToken.sol
@@ -6,7 +6,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
 contract SampleToken is ERC20, ERC165 {
-    constructor(uint256 initialSupply) ERC20("Sample", "SMP") {
-        _mint(msg.sender, initialSupply);
-    }
+  constructor(uint256 initialSupply) ERC20("Supports", "SUP") {
+    _mint(msg.sender, initialSupply);
+  }
+
+  function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+    return interfaceId == type(IERC20).interfaceId || super.supportsInterface(interfaceId);
+  }
 }

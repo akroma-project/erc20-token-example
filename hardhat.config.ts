36d3bf7d73517ffb73c2f0f091b66daad4ef9bf3
Eric Polerecky <eric@polerecky.com>
2022-10-05 10:30:37 -0400

    chore: remove require statement

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
-require("hardhat-abi-exporter");
+import "hardhat-abi-exporter";
 
 dotenv.config();
 
